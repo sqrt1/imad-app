bd7642c76ef63d847c9a47093d8982f87d8f4df5
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-06 23:10:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-   res.sendFile('This is article one'); 
+   res.send('This is article one'); 
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
