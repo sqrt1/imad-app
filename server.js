1b0098a2e98ea475a3fc2d0597bd79b63d75790d
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-15 21:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res){
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
