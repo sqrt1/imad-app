a9c5b61967a6d7d9adfcf8d969e01c79a2e279d9
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-17 00:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req,res){
+   counter = counter +1;
+   res.send(counter.toString()); 
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
