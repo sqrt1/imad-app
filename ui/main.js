6e049426f6ed2bdd339a2561e7e05166c8305782
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-16 22:52:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft +'px';
+}
 var img= document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval  = setInterval(moveRight, 100);
     
 };
