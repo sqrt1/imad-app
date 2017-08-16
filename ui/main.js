3ac8b4e897fca45aaf0d175075ac9f014377d674
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-16 22:54:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,9 @@ element.innerHTML='New value';
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    marginLeft = marginLeft +'px';
+    img.style.marginLeft = marginLeft +'px';
 }
 var img= document.getElementById('madi');
 img.onclick = function(){
     var interval  = setInterval(moveRight, 100);
-    
 };
