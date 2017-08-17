e85d4e5932a1801159a8c3cf9776ddd696ae2c31
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-17 23:46:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,6 @@ button.onclick = function(){
             }
         }
     };
-  request.open('GET','http://http://anshumanupadhyay1.imad.hasura-app.io/counter', true);
+  request.open('GET','http://anshumanupadhyay1.imad.hasura-app.io/counter', true);
   request.send(null);
 };
