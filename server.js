6e50e9d771fccb95cdf46e8af19ff5da44868909
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-20 16:45:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/articles/:articleName', function(req, res){
 });
 app.get('/articles/:articleName', function (req, res){
     var articleName = req.params.articleName;
-    pool.query("SELECT * FROM article where title = "+articleName+'', function(err, result){
+    pool.query("SELECT * FROM article where title = ''"+articleName+'', function(err, result){
        if(err){
            res.status(500).send(err.toString());
        }else {
