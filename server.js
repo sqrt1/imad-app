68a51128d953d23508a6f07f50da0b076bae1d54
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-23 00:44:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/articles/:articleName', function (req, res){
 });
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return hashed.toString();
 }
 
 app.get('/hash/:input', function(req, res){
