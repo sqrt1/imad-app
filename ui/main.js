b75a57967eadaa99a714a4961f32f145d2399e62
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-25 02:18:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ submit.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-      if(request.readyState == XMLHttpRequest.DONE){
+      if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
           console.log('user logged in');
           alert('Logged in successfully');
@@ -23,5 +23,6 @@ submit.onclick = function(){
     console.log(username);
     console.log(password);
     request.open('POST','http://anshumanupadhyay1.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username, password:password}));
 };
