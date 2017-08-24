a987a1d87f179869f1fd1509a61ac4201cf5b62b
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-25 01:16:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,27 @@
-var button = document.getElementById('counter');
-button.onclick = function(){
-    
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState == XMLHttpRequest.DONE){
-            if(request.status == 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-  request.open('GET','http://anshumanupadhyay1.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
 
-var nameInput = document.getElementbyId('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list +='<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+      if(request.readyState == XMLHttpRequest.DONE){
+          if(request.status === 200){
+          console.log('user logged in');
+          alert('Logged in successfully');
+          }
+         else if(request.status === 403)
+              alert('Incorrect username/password');
+          else if(request.status ===500)
+          alert('Something went wrong on the server');
+      }  
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://anshumanupadhyay1.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username:username, password:password}));
 };
