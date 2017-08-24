8929699988803b7b3cf54ca882cd670df9fad6c6
sqrt1 <anshumanupadhyay1@gmail.com>
2017-08-24 23:14:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ function createTemplate(data){
  app.post('/create-user', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
-    var salt = crypto.randomBytes(128).toString();
+    var salt = crypto.randomBytes(128).toString('hex');
     var hashedDbPassword = hash(password, salt); 
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, hashedDbPassword], function(err, result){
        if(err){
